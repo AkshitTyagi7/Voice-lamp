047d4418b655e6c9f3520e3e1fa7c4e025c3dec0
AkshitTyagi7 <102504167+AkshitTyagi7@users.noreply.github.com>
2022-12-04 00:49:46 +0530

    Update api.py

--- api.py
+++ api.py
@@ -20,16 +20,8 @@ async def read_root(request: Request):
     payload = await request.json()
     print(payload)
     if 'message' in payload:
-        res = main.gpt3(payload['message'])
+        res = main.main(payload['message'])
         print(res)
         return {
-            'result': res
-        }
-@app.post("/uploadfile/")
-async def create_upload_file(
-    file: UploadFile = File(description="A file read as UploadFile"),
-):
-    res=main.main(file)
-    return {
             'result': res
         }
